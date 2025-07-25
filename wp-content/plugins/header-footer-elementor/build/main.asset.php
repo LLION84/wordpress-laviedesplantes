c8873060ead4f74177cad7156ea5a240bb189114
LLION84 <blaisemohamed@outlook.fr>
2025-07-25 12:29:23 +0200

    Ajout des captures d’écran
    Ajout des captures d’écran

--- wp-content/plugins/header-footer-elementor/build/main.asset.php
+++ wp-content/plugins/header-footer-elementor/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a4bf903159352f100bfb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '183a118202c0f7a34325');
