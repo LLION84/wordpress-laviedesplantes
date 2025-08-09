1981135b9042af1c4cd076b1c160ea6e7178b9ac
LLION84 <blaisemohamed@outlook.fr>
2025-08-09 04:44:14 +0200

    Remplacement capture écran fichier

--- wp-content/plugins/wpforms-lite/vendor/composer/installed.php
+++ wp-content/plugins/wpforms-lite/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'awesomemotive/wpforms',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '871049bd642a06525c75072bda8520473fc2fbe4',
+        'reference' => '750e6b54be53cc01e1e7d51557bd92abc4ee2c1a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -49,7 +49,7 @@
         'awesomemotive/wpforms' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '871049bd642a06525c75072bda8520473fc2fbe4',
+            'reference' => '750e6b54be53cc01e1e7d51557bd92abc4ee2c1a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -103,7 +103,7 @@
         'roave/security-advisories' => array(
             'pretty_version' => 'dev-latest',
             'version' => 'dev-latest',
-            'reference' => 'a76f62e135c8b583602bd99df737b5c20f4d7200',
+            'reference' => '90fce983a76dd90a53a5b8171a98421aa703d58d',
             'type' => 'metapackage',
             'install_path' => null,
             'aliases' => array(
@@ -184,9 +184,9 @@
             'dev_requirement' => false,
         ),
         'woocommerce/action-scheduler' => array(
-            'pretty_version' => '3.9.2',
-            'version' => '3.9.2.0',
-            'reference' => 'efbb7953f72a433086335b249292f280dd43ddfe',
+            'pretty_version' => '3.9.3',
+            'version' => '3.9.3.0',
+            'reference' => 'c58cdbab17651303d406cd3b22cf9d75c71c986c',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
             'aliases' => array(),
